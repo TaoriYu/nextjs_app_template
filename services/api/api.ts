33005c52083d5a64431cefabfe22fbdad0fcb4df
Aleksandr Kuznetsov <d100@me.com>
2018-12-14 12:49:00 +0300

    refactor(services): переименовать provide в makeStore для читабильности
    Добавил makeStore в api для inheritance

--- services/api/api.ts
+++ services/api/api.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import debug from 'debug';
+import { unmanaged } from 'inversify';
 import { IConfigFields } from '../../config/types/IConfig';
 import { publicConfig } from '../../config/utils/publicConfig';
+import { makeStore } from '../provider/MakeStore';
 
+@makeStore(Api, { singletonScope: false })
 export class Api {
   protected readonly api: AxiosInstance;
   private apiDebug = debug(this.constructor.name);
 
-  public constructor(keyOrInstance?: keyof IConfigFields['apis'] | AxiosInstance) {
+  public constructor(@unmanaged() keyOrInstance?: keyof IConfigFields['apis'] | AxiosInstance) {
     if (typeof keyOrInstance === 'string' || typeof keyOrInstance === 'number') {
       this.api = axios.create({ ...publicConfig('apis')[keyOrInstance] });
     } else if (typeof keyOrInstance !== 'undefined') {
