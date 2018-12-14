50a5474f49e502f53e213ffd983492495ebf8dec
Aleksandr Kuznetsov <d100@me.com>
2018-12-14 12:29:47 +0300

    feat(services): инстанциировать api из строки или инстанса axios

--- services/api/api.ts
+++ services/api/api.ts
@@ -1,15 +1,20 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import debug from 'debug';
+import { IConfigFields } from '../../config/types/IConfig';
 import { publicConfig } from '../../config/utils/publicConfig';
 
 export class Api {
   protected readonly api: AxiosInstance;
   private apiDebug = debug(this.constructor.name);
 
-  public constructor(api?: AxiosInstance) {
-    this.api = api
-      ? api
-      : axios.create({ ...publicConfig('apis').defaultApi });
+  public constructor(keyOrInstance?: keyof IConfigFields['apis'] | AxiosInstance) {
+    if (typeof keyOrInstance === 'string' || typeof keyOrInstance === 'number') {
+      this.api = axios.create({ ...publicConfig('apis')[keyOrInstance] });
+    } else if (typeof keyOrInstance !== 'undefined') {
+      this.api = keyOrInstance;
+    } else {
+      this.api = axios.create({ ...publicConfig('apis').defaultApi });
+    }
     this.api.interceptors.request.use(this.requestLogger);
     this.api.interceptors.response.use(this.responseLogger);
   }
