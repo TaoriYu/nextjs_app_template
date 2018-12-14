e1bc4cc128efe1e534d066e6dbc8577769a7738f
Aleksandr Kuznetsov <d100@me.com>
2018-12-14 19:22:38 +0300

    docs(stores): добавить документацию для API

--- stores/api/api.ts
+++ stores/api/api.ts
@@ -3,11 +3,25 @@ import debug from 'debug';
 import { unmanaged } from 'inversify';
 import { IConfigFields } from '../../config/types/IConfig';
 import { publicConfig } from '../../config/utils/publicConfig';
-import { provide } from '../provider/provide';
 import { Transit } from './transit';
 
-@provide(Api)
-export class Api extends Transit {
+/**
+ * Class for working with http API, provide and instantiate axios instance from application
+ * configuration
+ * @see Transit
+ * @example
+ * class MyHttpRequester extends Api {
+ *   public async getData(): IncomingSomeDTO {
+ *     const response = await this.api.get('/data');
+ *     return this.toDTO(IncomingSomeDTO, response.data);
+ *   }
+ *
+ *   public async saveData(params: any): void {
+ *     await this.api.post('/data', this.toDTO(OutgoingSomeDTO, params))
+ *   }
+ * }
+ */
+export abstract class Api extends Transit {
   protected readonly api: AxiosInstance;
   /** use evn DEBUG=Api:* npm run dev */
   private apiDebug = debug(`Api:${this.constructor.name}`);
