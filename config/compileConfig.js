797b9161b328c10905b932351f7c9bbcb140f285
Aleksandr Kuznetsov <d100@me.com>
2018-12-14 14:55:36 +0300

    fix(configs): сборка должна оповещать об ошибках
    Проект не собирается если не собрались конфиги
    Теперь сборка конфига тригирит сборку конфига а не
    проекта

--- config/compileConfig.js
+++ config/compileConfig.js
@@ -4,7 +4,12 @@
  */
 module.exports = function compileConfig() {
   const spawnSync = require('child_process').spawnSync;
-  spawnSync('tsc', ['-b', '--force', './']);
+  const { stdout } = spawnSync('tsc', ['-b', '--force', './config/']);
+  if (stdout.toString().match('error')) {
+    throw new Error(stdout.toString());
+  } else {
+    console.log('configuration compiled successfully')
+  }
   const env = process.env.NODE_ENV || 'development';
   return require(`./dist/${env}.config.js`).default;
 }
