7efb6c4c3d6d45ee68d20aa036ee802918deff0c
Aleksandr Kuznetsov <d100@me.com>
2018-12-16 21:05:55 +0300

    feat(product): add mobx and mobx-react to deps
    Also add github api to config

--- config/api/defaultApi.ts
+++ config/api/defaultApi.ts
@@ -1,7 +1,7 @@
 import { IApi } from '../types/IConfig';
 
 export const defaultApi: IApi = {
-  baseURL: 'http://localhost:4100',
-  timeout: 200,
+  baseURL: 'http://api.github.com',
+  timeout: 2000,
   public: true,
 };
