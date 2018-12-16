f6988e66aaea4e645203d43162529dea208747cd
Aleksandr Kuznetsov <d100@me.com>
2018-12-16 23:15:22 +0300

    feat(stores): add users search logic

--- stores/users/index.ts
+++ stores/users/index.ts
@@ -1,1 +1,2 @@
 export * from './users.store';
+export * from './user';
--- stores/users/users.store.ts
+++ stores/users/users.store.ts
@@ -1,6 +1,22 @@
+import { action, observable } from 'mobx';
+import { Api } from '../api';
 import { makeStore } from '../provider/MakeStore';
+import { UsersDto } from './dto';
+import { User } from './user';
 
 @makeStore(UsersStore)
-export class UsersStore {
+export class UsersStore extends Api {
+  @observable public users: User[] = [];
+  @observable public totalCount: number = 0;
 
+  @action public async searchUsers(query: string) {
+    const { data } = await this.api.get('/search/users', {
+      params: {
+        q: query,
+        order: 'desc'
+      }
+    });
+
+    this.users = this.toDTO(UsersDto, data).items.map((item) => new User().fromDTO(item));
+  }
 }
