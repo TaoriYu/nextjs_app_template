f9d83509d6280fd576ec403d512cef8d5c70c7c6
Aleksandr Kuznetsov <d100@me.com>
2018-12-22 17:20:20 +0300

    fix(typescript): fixed typechecking errors

--- utils/tests/axiosMockFactory.ts
+++ utils/tests/axiosMockFactory.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+// @ts-ignore ignored cause config is sub project
 import { appConfig } from '../../config/config';
 
 export function axiosMockFactory(responseFactory: (config: AxiosRequestConfig) => Promise<AxiosResponse>) {
