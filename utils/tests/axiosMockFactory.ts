42c2e8f4450f8fca0f384ae81cef243ca5907fbc
Aleksandr Kuznetsov <d100@me.com>
2018-12-23 18:59:07 +0300

    test(stores): add tests to the API class and refactor mock factory

--- utils/tests/axiosMockFactory.ts
+++ utils/tests/axiosMockFactory.ts
@@ -1,9 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-// @ts-ignore ignored cause config is sub project
-import { appConfig } from '../../config/config';
+import { publicConfig } from '../../config/utils/publicConfig';
 
 export function axiosMockFactory(responseFactory: (config: AxiosRequestConfig) => Promise<AxiosResponse>) {
-  const { defaultApi } = appConfig.publicRuntimeConfig.apis;
+  const { defaultApi } = publicConfig('apis');
   return axios.create({
     ...defaultApi,
     adapter: (config) => {
