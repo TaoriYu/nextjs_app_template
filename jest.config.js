c6f1f3918a90d8c82682994c2e93c42836d2bb7e
Aleksandr Kuznetsov <d100@me.com>
2018-12-23 18:57:36 +0300

    test(tests): add global config mock

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFiles: ['./stores/provider/container.ts'],
+  setupFiles: [
+    './stores/provider/container.ts',
+    './utils/tests/configMock.ts',
+  ],
   globals: {
     'ts-jest': {
       isolatedModules: true
