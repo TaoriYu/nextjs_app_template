6897e82288fdd4dfba27cd3a297d96188fef4b54
oklimonova <o.klimonova@cian.ru>
2018-12-28 16:53:58 +0300

    style(utils): исправить код в соответствии с tslint

--- utils/build/withTypescript.js
+++ utils/build/withTypescript.js
@@ -20,6 +20,7 @@ module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       const path = require('path')
+
       if (!options.defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
