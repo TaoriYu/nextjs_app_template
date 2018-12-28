23eb433d0ae0947f1e159ac9156da45608feb8ae
oklimonova <o.klimonova@cian.ru>
2018-12-28 16:38:23 +0300

    style(stores): исправить код в соответствии с линтом

--- stores/api/api.ts
+++ stores/api/api.ts
@@ -28,6 +28,7 @@ export abstract class Api extends Transit {
 
   public constructor(@unmanaged() keyOrInstance?: keyof IConfigFields['apis'] | AxiosInstance) {
     super();
+
     if (typeof keyOrInstance === 'string' || typeof keyOrInstance === 'number') {
       this.api = axios.create({ ...publicConfig('apis')[keyOrInstance] });
     } else if (typeof keyOrInstance !== 'undefined') {
@@ -35,25 +36,30 @@ export abstract class Api extends Transit {
     } else {
       this.api = axios.create({ ...publicConfig('apis').defaultApi });
     }
+
     this.api.interceptors.request.use(this.requestLogger);
     this.api.interceptors.response.use(this.responseLogger);
   }
 
   private requestLogger = (config: AxiosRequestConfig) => {
     const { timeout, baseURL, headers, data, method, params, url } = config;
+
     this.apiDebug('==request==');
     this.apiDebug({ timeout, baseURL, headers, data, method, params, url });
     this.apiDebug('====end====');
+
     return config;
   }
 
   private responseLogger = (response: AxiosResponse) => {
     const { baseURL, url } = response.config;
+
     this.apiDebug('==response==');
     this.apiDebug({ baseURL, url });
     this.apiDebug(response.headers);
     this.apiDebug(response.data);
     this.apiDebug('====end====');
+
     return response;
   }
 }
